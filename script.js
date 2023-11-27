4dbe6e689f4a7a49790d6d738856669db59fb0e9
Mustafa Merchant <101447345+MustafaMerchant21@users.noreply.github.com>
2023-11-27 15:47:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ AOS.init({
   duration: 1000, 
   once: true,
 });
+ window.addEventListener('scroll', function (){
+    let header = document.querySelector('header');
+    let windowPosition = window.scrollY > 0;
+    header.classList.toggle('scrollingActive', windowPosition);
+})
